9f4aa16481460e0ec50bbbba463d472af643eb69
wei <1216381706@qq.com>
2022-03-29 19:29:14 +0800

    害，咱又来学java了，fighting

---
+++ day11/src/main/java/ExtendsTest.java
@@ -0,0 +1,11 @@
+public class ExtendsTest {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.name = "wei";
+        p1.sleep();
+        System.out.println(p1.name);
+
+        Student s1 = new Student();
+        s1.eat();
+    }
+}
