731d1326f809dcab68ca16284013e7ef4146bcf1
wei <1216381706@qq.com>
2022-04-30 23:48:13 +0800

    oa实战开始，这一次的Mybatis工具类结合函数式编程Lambda表达式更为简便，希望能学到更多

---
+++ OA/imooc-oa/src/test/java/com/imooc/oa/utils/MybatisUtilsTestor.java
@@ -0,0 +1,23 @@
+package com.imooc.oa.utils;
+
+import org.junit.Test;
+
+
+public class MybatisUtilsTestor {
+    @Test
+    public void testcase1() {
+        String result = (String) MybatisUtils.executeQuery(sqlSession -> {
+            String out = sqlSession.selectOne("test.sample");
+            //这里的return out; 返回的是MybatisUtils.java里的Object obj，
+            // 所以如果需要String类型的话就需要强制类型转换
+            return out;
+        });
+        System.out.println(result);
+    }
+
+    @Test
+    public void testcase2() {
+        String result = (String) MybatisUtils.executeQuery(sqlSession ->sqlSession.selectOne("test.sample"));
+        System.out.println(result);
+    }
+}
