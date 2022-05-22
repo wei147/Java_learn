0251822fa58c3b5ef29c7ca0329094b9bd831827
wei <1216381706@qq.com>
2022-05-22 23:03:09 +0800

    截止今日，OA系统的请假流程功能已经完成。但基于最近的学习态度和进度，这个环节还是有一些不懂

--- OA/imooc-oa/src/main/java/com/imooc/oa/controller/ForwardServlet.java
+++ OA/imooc-oa/src/main/java/com/imooc/oa/controller/ForwardServlet.java
@@ -5,15 +5,25 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "ForwardServlet", urlPatterns = "/ForwardServlet")
+/**
+ * 页面跳转Servlet
+ */
+@WebServlet(name = "ForwardServlet", urlPatterns = "/forward/*")
 public class ForwardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uri = request.getRequestURI();   //获取uri的地址
+        System.out.println("uri是 ---=-"+uri);
+        /**
+         * /forward/form
+         * /forward/a/b/c/form
+         */
+        String subUri = uri.substring(1);    //代表从第二个字符之后开始截取   forward/form
+        String page = subUri.substring(subUri.indexOf("/")); // a/b/c/form
+        System.out.println("page 是-----====="+ page);
+        request.getRequestDispatcher(page + ".ftl").forward(request, response); //这个不理解
+
 
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
