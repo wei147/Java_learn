6162d341b4152cd322d4febe06068b0676eb0e52
wei <1216381706@qq.com>
2022-05-23 23:54:41 +0800

    多表联合查询xml与

--- OA/imooc-oa/src/main/java/com/imooc/oa/dao/LeaveFromDao.java
+++ OA/imooc-oa/src/main/java/com/imooc/oa/dao/LeaveFromDao.java
@@ -1,7 +1,16 @@
 package com.imooc.oa.dao;
 
 import com.imooc.oa.entity.LeaveForm;
+import com.imooc.oa.entity.ProcessFlow;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 public interface LeaveFromDao {
     public void insert(LeaveForm form);
+
+    //    public List<Map> selectByParams( Map params); 不推荐
+    //通过@Param 说明在他们xml中的参数名是什么
+    public List<Map> selectByParams(@Param("pf_state") String pfState, @Param("pf_operator_id") Long operatorId);
 }
--- OA/imooc-oa/src/test/java/com/imooc/oa/dao/LeaveFromDaoTest.java
+++ OA/imooc-oa/src/test/java/com/imooc/oa/dao/LeaveFromDaoTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -35,6 +37,16 @@ public class LeaveFromDaoTest {
             form.setState("processing");    //当前状态
             dao.insert(form);
             return null;
+        });
+    }
+
+    @Test
+    public void selectByParams() {
+        MybatisUtils.executeQuery(sqlSession -> {
+            LeaveFromDao dao = sqlSession.getMapper(LeaveFromDao.class);
+            List<Map> list = dao.selectByParams("process", 2L);
+            System.out.println(list);   //这里打印的字段名是和数据表的字段名一致的（原始字段名）。因为是通过多表查询的，后面使用也要用该字段名 {start_time=2022-05-22 00:00:00.0, reason=我有事,要请假, create_time=2022-05-22 23:18:33.0, department_id=2, employee_id=4, department_name=研发部, form_id=26, end_time=2022-06-23 00:00:00.0, name=宋彩妮, form_type=1, state=processing}, {start_time=2022-05-23 00:00:00.0, reason=有事，我请一天假, create_time=2022-05-23 10:59:57.0, department_id=2, employee_id=4, department_name=研发部, form_id=27, end_time=2022-05-23 23:00:00.0, name=宋彩妮, form_type=1, state=processing}
+            return list;
         });
     }
 }
