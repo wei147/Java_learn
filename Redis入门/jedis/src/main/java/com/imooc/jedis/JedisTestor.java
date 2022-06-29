42a216ac4959ce4fd1fd89ff444fff8b40eebbb0
wei <1216381706@qq.com>
2022-06-29 23:35:38 +0800

    日常提交

---
+++ Redis入门/jedis/src/main/java/com/imooc/jedis/JedisTestor.java
@@ -0,0 +1,53 @@
+package com.imooc.jedis;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class JedisTestor {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.10.102", 6379);
+        try {
+            jedis.auth("1234");
+            jedis.select(2);
+
+//            Set<String> str = jedis.keys("*");
+//            System.out.println(str);
+//            List<String> value = jedis.lrange("listkey", 0, -1);
+//            System.out.println(value);
+            System.out.println("redis 连接成功");
+            //字符串
+            jedis.set("sn", "10086");
+            String sn = jedis.get("sn");
+            System.out.println(sn);
+            jedis.mset(new String[]{
+                    "title", "迎击", "action", "铁马冰河入梦来", "time", "公元1200"
+            });
+            List<String> list = jedis.mget(new String[]{"sn", "action", "title", "time"});
+            Long num = jedis.incr("sn");    //自增 +1
+            System.out.println(num);
+            System.out.println(list);
+
+            //Hash
+            jedis.hset("student:1433", "name", "小飞");
+            String student = jedis.hget("student:1433", "name");  //要传入两个参数，key和属性才能拿到值
+            System.out.println(student);
+            Map<String, String> studentMap = new HashMap();
+            studentMap.put("name", "火狐");
+            studentMap.put("age", "19");
+            studentMap.put("id", "2000");
+            jedis.hmset("student:1999", studentMap);
+            //这里的两个String是因为Map数据类型的键和值都需要转为String类型？
+            Map<String, String> text = jedis.hgetAll("student:1999");
+            System.out.println(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            jedis.close();
+        }
+
+    }
+}
