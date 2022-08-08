42ce0c0ba4e9fbc9cfb1e796016f4a8d42dbea7f
wei <1216381706@qq.com>
2022-08-09 03:07:49 +0800

    卸载文明6，一切正常，声明式事务。今天啥也没敲

---
+++ Spring基础/第3节 Spring JDBC与事务管理/s02/src/main/java/com/imooc/spring/jdbc/service/EmployeeService.java
@@ -0,0 +1,30 @@
+package com.imooc.spring.jdbc.service;
+
+import com.imooc.spring.jdbc.dao.EmployeeDao;
+import com.imooc.spring.jdbc.entity.Employee;
+import java.util.Date;
+
+public class EmployeeService {
+    private EmployeeDao employeeDao;
+
+    public void batchImport() {
+        for (int i = 1; i <= 10; i++) {
+            Employee employee = new Employee();
+            employee.setEno(8000 + i);
+            employee.setEname("员工" + i);
+            employee.setSalary(4000f);
+            employee.setDname("市场部");
+            employee.setHiredate(new Date());
+            employeeDao.insert(employee);
+        }
+    }
+
+    public EmployeeDao getEmployeeDao() {
+        return employeeDao;
+    }
+
+    public void setEmployeeDao(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+}
