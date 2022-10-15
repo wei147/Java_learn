6179dd103bfabadee42da82f2d350bd113732b53
wei <1216381706@qq.com>
2022-10-15 23:56:31 +0800

    今天啥也没看,软考复习开始,无聊的周末上课

--- OA_spring/src/main/java/com/wei/oa_spring/config/OAWebMvcConfig.java
+++ OA_spring/src/main/java/com/wei/oa_spring/config/OAWebMvcConfig.java
@@ -1,6 +1,6 @@
-package com.imooc.mall.config;
+package com.wei.oa_spring.config;
 
-import com.imooc.mall.common.Constant;
+import com.wei.oa_spring.common.Constant;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 配置地址映射
  */
 @Configuration
-public class ImoocMallWebMvcConfig implements WebMvcConfigurer {
+public class OAWebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
@@ -19,8 +19,8 @@ public class ImoocMallWebMvcConfig implements WebMvcConfigurer {
 //                "classpath:/META-INF/resources/webjars/");
 
         //关于上传图片的自定义静态资源映射目录  (就是说http://localhost/images/uuid.png 对应的就是本地 FILE_UPLOAD_DIR的文件路径)
-        registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:" + Constant.FILE_UPLOAD_DIR);
+//        registry.addResourceHandler("/images/**")
+//                .addResourceLocations("file:" + Constant.FILE_UPLOAD_DIR);
 
         //以下都是swagger的静态资源映射? （自动生成api文档）
         registry.addResourceHandler("/**").addResourceLocations(
--- OA_spring/src/main/java/com/wei/oa_spring/controller/LeaveFormController.java
+++ OA_spring/src/main/java/com/wei/oa_spring/controller/LeaveFormController.java
@@ -9,11 +9,7 @@ import com.wei.oa_spring.model.request.AuditProcessFlowReq;
 import com.wei.oa_spring.model.request.CreateLeaveFormReq;
 import com.wei.oa_spring.service.LeaveFormService;
 import org.springframework.beans.BeanUtils;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
@@ -21,8 +17,8 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
-@Controller
-@RequestMapping("/leaveForm/")
+@RestController
+@RequestMapping("leaveForm/")
 public class LeaveFormController {
 
     @Resource
@@ -41,7 +37,7 @@ public class LeaveFormController {
     }
 
     //查询需要审核的请假单列表
-    @GetMapping("/list")
+    @GetMapping("list")
     public ApiRestResponse getLeaveFormList(HttpSession session) {
         //1.接收各项请假单数据
         User user = (User) session.getAttribute(Constant.OA_USER);
@@ -55,7 +51,7 @@ public class LeaveFormController {
 
     //具体实现类没搞好
     //审批操作
-    @GetMapping("/audit")
+    @GetMapping("audit")
     public ApiRestResponse audit(HttpSession session, @Valid @RequestBody AuditProcessFlowReq auditProcessFlowReq) {
         //这个登录判断后面可以仿 统一校验管理员身份,做一个统一校验登录
         ProcessFlow processFlow = new ProcessFlow();
